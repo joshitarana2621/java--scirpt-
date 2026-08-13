814200e09e230bc0380d676eb69bd3f16ad29277
tarana joshi <joshitarana26@gmail.com>
2026-08-13 18:06:23 +0530

    Add files via upload

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all code as js newer version;
+
+//alert(3+3) it is only allowed when we use browser , not in node js
+
+console.log(3
+    +3) // code should be readable
+
+console.log(3+3)
+
+let name = "hello" //string
+let age = 18 //int
+let isLoggedIn = true //bool
+
+//number => 2 to power 53
+//bigInt (mainly used when input is bigger than number example: redHert,trading)
+//string => ""
+//bool => true/false
+//Null => standlone value(representation of empty value)
+//Undefined => currently value is not assigned
+//symbols =>unique
+
+//object
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
