7a806d257451f7d710aba3ce0c5aac8c353992d7
tarana joshi <joshitarana26@gmail.com>
2026-08-13 21:57:49 +0530

    Add files via upload

---
+++ 01_basics/01_conversionOperations.js
@@ -0,0 +1,26 @@
+let score = "33";
+
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33;
+//"33abc" =>NaN(NOT AN NUMBER)
+//string,undefined => NaN
+//true =>1,false=> 0;
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1   => true 0=>false;
+//" "  =>false;
+//"name" ="true";
+
+let n = 33;
+let stringNumber = String(n);
+console.log(stringNumber);
+console.log(typeof stringNumber);
